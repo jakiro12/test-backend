9a96e1eff24b5b0dd91f04cabac66e76efb3d590
Jakiro12 <eclipsisnocte@gmail.com>
2023-02-15 11:22:10 -0300

    what

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const counterWebReload = require('./countViews/counterUsers')
 const port= process.env.PORT || 4000
 app.listen(port)
 app.get('/',(req,res)=>{
+    console.log('hola')
     res.send('hola a todos')
     counterWebReload()
 })
